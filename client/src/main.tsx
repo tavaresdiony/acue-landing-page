4332a24ed8edf220cf2c5d94b2c00a4317c845f4
tavaresdiony <dionathatavares19@gmail.com>
2026-05-22 21:34:45 -0300

    Integrate PostHog for analytics in main.tsx
    Initialize PostHog for analytics tracking and register app details.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,21 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import posthog from "posthog-js";
+
+posthog.init(
+  "phc_CEdbVrcP8cEuWMAZMsCfnUJuZvSugKnYxgqARA25XrPs",
+  {
+    api_host: "https://app.posthog.com",
+    capture_pageview: true,
+    capture_pageleave: true,
+    autocapture: true,
+  }
+);
+
+posthog.register({
+  app_domain: "site.acueapp.com.br",
+  app_type: "landing",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
